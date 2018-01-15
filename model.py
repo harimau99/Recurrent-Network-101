1efa15702d20a297cb1abcfe78ff693c8448683c
HUSEIN ZOLKEPLI <husein.zol05@gmail.com>
2018-01-15 10:48:22 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 import numpy as np
 
 class Model:
-    
+
     def __init__(self, num_layers, size_layer, enable_dropout, dropout, enable_penalty, delta, activation_function,
                  loss_function, learning_rate, size_input, size_output, optimizer, gate_type):
-        
+
         self.X = tf.placeholder(tf.float32, (None, None, size_input))
         self.Y = tf.placeholder(tf.float32, (None, size_output))
-        
+
         def define_cell(model, size_layer, activation):
             if activation == 'sigmoid':
                 activation = tf.nn.sigmoid
@@ -25,9 +25,9 @@ class Model:
                 return tf.nn.rnn_cell.GRUCell(size_layer, activation = activation)
             else:
                 raise Exception("gate type not supported")
-        
+
         def chooseoptimizer(ops):
-            
+
             if ops == 'gradientdescent':
                 return tf.train.GradientDescentOptimizer
             elif ops == 'adagrad':
@@ -38,26 +38,24 @@ class Model:
                 return tf.train.RMSPropOptimizer
             else:
                 raise Exception("sgd technique not supported")
-            
+
         def calculate_cost(loss, y_hat, y):
-                
+
             if loss == 'MSE':
-                return tf.reduce_mean(tf.square(y_hat - y))
+                return tf.reduce_mean(tf.square(y - y_hat))
             elif loss == 'RMSE':
-                return tf.sqrt(tf.reduce_mean(tf.square(y_hat - y)))
+                return tf.sqrt(tf.reduce_mean(tf.square(y - y_hat)))
             elif loss == 'MAE':
-                return tf.reduce_mean(tf.abs(y_hat - y))
-        
+                return tf.reduce_mean(tf.abs(y - y_hat))
+
         rnn_cells = tf.nn.rnn_cell.MultiRNNCell([define_cell(gate_type, size_layer, activation_function) for _ in range(num_layers)], state_is_tuple = False)
-        if gate_type == 'lstm': 
+        if gate_type == 'lstm':
             self.hidden_layer = tf.placeholder(tf.float32, (None, num_layers * 2 * size_layer))
         else:
             self.hidden_layer = tf.placeholder(tf.float32, (None, num_layers * size_layer))
         if enable_dropout:
-            drop = tf.contrib.rnn.DropoutWrapper(rnn_cells, output_keep_prob = dropout)
-            self.outputs, self.last_state = tf.nn.dynamic_rnn(drop, self.X, initial_state = self.hidden_layer, dtype = tf.float32)
-        else:
-            self.outputs, self.last_state = tf.nn.dynamic_rnn(rnn_cells, self.X, initial_state = self.hidden_layer, dtype = tf.float32)
+            rnn_cells = tf.contrib.rnn.DropoutWrapper(rnn_cells, output_keep_prob = dropout)
+        self.outputs, self.last_state = tf.nn.dynamic_rnn(rnn_cells, self.X, initial_state = self.hidden_layer, dtype = tf.float32)
         rnn_W = tf.Variable(tf.random_normal((size_layer, size_output)))
         rnn_B = tf.Variable(tf.random_normal([size_output]))
         self.logits = tf.matmul(self.outputs[-1], rnn_W) + rnn_B
@@ -66,6 +64,6 @@ class Model:
         if enable_penalty:
             l2 = sum(delta * tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
             self.cost += l2
-            
+
         grad_descent = chooseoptimizer(optimizer)
         self.optimizer = grad_descent(learning_rate).minimize(self.cost)
